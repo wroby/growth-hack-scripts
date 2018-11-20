1dd30df3294ea87f7dd88d79eb242e6783e1c396
wroby <twrobel77@gmail.com>
2018-11-20 18:21:31 +0100

    Create facebook-auto-like.js

---
+++ facebook-auto-like.js
@@ -0,0 +1,18 @@
+/* *******************************************************************************************
+* This hack allows you to automatically like every posts and comments of a page.
+*Go to a specific Facebook page or post
+*
+* -> Open the Chrome console (Command + Option + J on Mac or Ctrl + Shift + J on Windows)
+*
+* -> Copy / Paste the script below:
+* ******************************************************************************************* */
+ 
+
+var inputs = document.getElementsByClassName('UFILikeLink');for(var i=0;i<inputs.length;i++){ inputs[i].click(); }
+
+ 
+/* *******************************************************************************************
+* Be careful, this script will like EVERY posts and comments of a specific page in a few seconds. 
+* Don't use this hack to much if you don't want Facebook to block you account.
+* ******************************************************************************************* */
+
